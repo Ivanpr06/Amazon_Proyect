68bd3b4ac20ed32e927cceaa2655349488b7dde9
IvánPonti <ivan.ponti.rubio06@gmail.com>
2025-11-23 03:57:05 +0100

    commit checkout
    empezamos pag checkout donde vemos los productos seleccionados, nueva funcion para el precio y en proceso de crear la funcion de eliminar producto del cart

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,45 @@
-const card = [];
+export let cart = [
+  {
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+},
+{
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}
+];
+
+
+export function addToCard(productId) {
+
+  let matchingItem;
+
+  cart.forEach((cardItem) => {
+    if (productId === cardItem.productId) {
+      matchingItem = cardItem;
+    }
+  });
+
+  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+  const quantity = Number(quantitySelector.value);
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: quantity
+    });
+  }
+}
+
+export function removeFromCard(productId) {
+  let newCard = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCard.push(cartItem);
+    };
+  });
+  cart = newCard;
+}
